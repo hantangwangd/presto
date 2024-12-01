15ca072ce1c649336af8b13d6480f742e3d4b1c9
wangd <mingwbd@gmail.com>
2024-12-01 22:54:56 +0800

    Execute optimization, segmentation and local planning for call distributed procedure

--- presto-main/src/main/java/com/facebook/presto/testing/LocalQueryRunner.java
+++ presto-main/src/main/java/com/facebook/presto/testing/LocalQueryRunner.java
@@ -445,7 +445,8 @@ public class LocalQueryRunner
                 new TablePropertyManager(),
                 new ColumnPropertyManager(),
                 new AnalyzePropertyManager(),
-                transactionManager);
+                transactionManager,
+                procedureRegistry);
         this.splitManager = new SplitManager(metadata, new QueryManagerConfig(), nodeSchedulerConfig);
         this.planCheckerProviderManager = new PlanCheckerProviderManager(new JsonCodecSimplePlanFragmentSerde(jsonCodec(SimplePlanFragment.class)), new PlanCheckerProviderManagerConfig());
         this.distributedPlanChecker = new PlanChecker(featuresConfig, false, planCheckerProviderManager);
